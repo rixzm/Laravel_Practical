501601cef1b62393f64904020ca06926f8b93456
rixzm <delacruz.joerix03@gmail.com>
2025-04-11 14:38:12 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
-Route::get('/products', [ProductController::class, 'index']);          // List all products
-Route::post('/products', [ProductController::class, 'store']);         // Create new product
-Route::get('/products{id}', [ProductController::class, 'show']);       // Show single product
-Route::put('/products{id}', [ProductController::class, 'update']);     // Update product
+Route::get('/products', [ProductController::class, 'index']); 
+Route::post('/products', [ProductController::class, 'store']);       
+Route::get('/products{id}', [ProductController::class, 'show']);      
+Route::put('/products{id}', [ProductController::class, 'update']);     
 Route::delete('/products{id}', [ProductController::class, 'destroy']); 
 
-Route::get('/categories', [CategoryController::class, 'index']);         // List all categories
-Route::post('/categories', [CategoryController::class, 'store']);        // Create new category
-Route::get('/categories{id}', [CategoryController::class, 'show']);      // Show single category
-Route::put('/categories{id}', [CategoryController::class, 'update']);    // Update category
+Route::get('/categories', [CategoryController::class, 'index']); 
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::get('/categories{id}', [CategoryController::class, 'show']); 
+Route::put('/categories{id}', [CategoryController::class, 'update']); 
 Route::delete('/categories{id}', [CategoryController::class, 'destroy']);
